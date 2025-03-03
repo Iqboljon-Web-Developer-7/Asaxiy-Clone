b450c77989a6c5aff05bc9e1e6a6d374ca33d04b
Iqboljon Ummataliyev <iqboljonummataliyev30@gmail.com>
2025-03-03 14:35:05 +0500

    created carousel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/about/About";
 import FamouseBrands from "@/components/famousBrends/FamousBrends";
+import Products from "@/components/Products/Products";
 
 const Home = () => {
   return (
     <div className="bg-slate-50 wrapper">
       <FamouseBrands />
       <About />
+      <Products />
     </div>
   );
 };
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api";
+import PermanentData from "./slices/permanentData";
 
 export const store = configureStore({
   reducer: {
+    PermanentData,
     [api.reducerPath]: api.reducer, // caching
   },
   middleware: (getDefaultMiddleware) =>
